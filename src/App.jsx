f5ad54bd8cd60939211d2c4dc04cf5fd4eccce89
Nenad <nenadmobmail@gmail.com>
2025-05-10 13:56:36 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { DashboardLayout, HomeLayout, Landing, Login, Register } from "./pages";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: "register",
+        element: <Register />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "dashboard",
+        element: <DashboardLayout />,
+      },
+    ],
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router}></RouterProvider>;
+};
+
+export default App;
